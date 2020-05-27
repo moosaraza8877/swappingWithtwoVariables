2b8db18cb0ea52169e4c15c41af62475b94741d1
moosaraza8877 <moosaraza8877@hotmail.com>
2020-05-27 21:36:30 +0500

    Ready to Commit

---
+++ Practise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter first value: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second value: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----- Before swapping -----");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("----- After swapping -----");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
